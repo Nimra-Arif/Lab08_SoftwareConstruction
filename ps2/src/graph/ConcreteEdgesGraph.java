1b308559ab90ff03a00fcf1a2b0763fe3951babd
Nimra-Arif <mahmad125197@gmail.com>
2023-11-19 09:21:23 +0500

    adding initial files

--- ps2/src/graph/ConcreteEdgesGraph.java
+++ ps2/src/graph/ConcreteEdgesGraph.java
@@ -10,7 +10,7 @@ import java.util.Map;
 import java.util.Set;
 
 /**
- * An implementation of Graph.
+ * An implementation of Graph with String-labeled vertices and weighted directed edges.
  * 
  * <p>PS2 instructions: you MUST use the provided rep.
  */
@@ -20,69 +20,201 @@ public class ConcreteEdgesGraph implements Graph<String> {
     private final List<Edge> edges = new ArrayList<>();
     
     // Abstraction function:
-    //   TODO
+    //   Represents a weighted directed graph with String-labeled vertices.
+    //   vertices: Set containing unique vertex labels.
+    //   edges: List containing directed edges between vertices, each with a weight.
+
     // Representation invariant:
-    //   TODO
+    //   No duplicate vertices in 'vertices' set.
+    //   Every Edge in 'edges' list has valid source and target vertices present in 'vertices'.
+
     // Safety from rep exposure:
-    //   TODO
+    //   - 'vertices' and 'edges' are private final fields.
+    //   - Return defensive copies in observer methods (vertices(), sources(), targets()).
     
-    // TODO constructor
     
-    // TODO checkRep
-    
-    @Override public boolean add(String vertex) {
-        throw new RuntimeException("not implemented");
+    /**
+     * Adds a vertex to the graph.
+     *
+     * @param vertex The vertex to be added.
+     * @return True if the vertex is added, false if it already exists.
+     */
+    @Override
+    public boolean add(String vertex) {
+        return vertices.add(vertex);
     }
     
-    @Override public int set(String source, String target, int weight) {
-        throw new RuntimeException("not implemented");
+    /**
+     * Adds or updates an edge in the graph.
+     *
+     * @param source The source vertex of the edge.
+     * @param target The target vertex of the edge.
+     * @param weight The weight of the edge.
+     * @return The previous weight of the edge, 0 if it's a new edge.
+     * @throws IllegalArgumentException if source or target vertices do not exist.
+     */
+    @Override
+    public int set(String source, String target, int weight) {
+        for (Edge edge : edges) {
+            if (edge.source.equals(source) && edge.target.equals(target)) {
+                int prevWeight = edge.weight;
+                edge.weight = weight;
+                return prevWeight;
+            }
+        }
+        edges.add(new Edge(source, target, weight));
+        return 0;
     }
     
-    @Override public boolean remove(String vertex) {
-        throw new RuntimeException("not implemented");
+    /**
+     * Removes a vertex from the graph.
+     *
+     * @param vertex The vertex to be removed.
+     * @return True if the vertex is removed, false if it doesn't exist.
+     */
+    @Override
+    public boolean remove(String vertex) {
+        return vertices.remove(vertex) && edges.removeIf(edge -> edge.source.equals(vertex) || edge.target.equals(vertex));
     }
     
-    @Override public Set<String> vertices() {
-        throw new RuntimeException("not implemented");
+    /**
+     * Returns all vertices in the graph.
+     *
+     * @return Set of vertices in the graph.
+     */
+    @Override
+    public Set<String> vertices() {
+        return new HashSet<>(vertices);
     }
     
-    @Override public Map<String, Integer> sources(String target) {
-        throw new RuntimeException("not implemented");
+    /**
+     * Returns sources that have an edge leading to the given target vertex.
+     *
+     * @param target The target vertex.
+     * @return Map of source vertices with their corresponding weights leading to the target.
+     */
+    @Override
+    public Map<String, Integer> sources(String target) {
+        Map<String, Integer> sourceMap = new HashMap<>();
+        for (Edge edge : edges) {
+            if (edge.target.equals(target)) {
+                sourceMap.put(edge.source, edge.weight);
+            }
+        }
+        return sourceMap;
     }
     
-    @Override public Map<String, Integer> targets(String source) {
-        throw new RuntimeException("not implemented");
+    /**
+     * Returns targets that can be reached from the given source vertex.
+     *
+     * @param source The source vertex.
+     * @return Map of target vertices with their corresponding weights reachable from the source.
+     */
+    @Override
+    public Map<String, Integer> targets(String source) {
+        Map<String, Integer> targetMap = new HashMap<>();
+        for (Edge edge : edges) {
+            if (edge.source.equals(source)) {
+                targetMap.put(edge.target, edge.weight);
+            }
+        }
+        return targetMap;
     }
     
-    // TODO toString()
+    /**
+     * Provides a string representation of the graph.
+     *
+     * @return String representation of the graph.
+     */
+    @Override
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+        str.append("Vertices: ").append(vertices).append("\n");
+        str.append("Edges: ").append(edges).append("\n");
+        return str.toString();
+    }
     
 }
 
 /**
- * TODO specification
- * Immutable.
- * This class is internal to the rep of ConcreteEdgesGraph.
+ * Represents an immutable edge in the graph.
  * 
  * <p>PS2 instructions: the specification and implementation of this class is
  * up to you.
  */
 class Edge {
     
-    // TODO fields
+    private final String source;
+    private final String target;
+    private int weight;
     
     // Abstraction function:
-    //   TODO
+    //   Represents a directed edge from 'source' vertex to 'target' vertex with 'weight'.
+    //
     // Representation invariant:
-    //   TODO
+    //   'source' and 'target' are non-null and non-empty strings.
+    //   'weight' is a positive integer.
+    //
     // Safety from rep exposure:
-    //   TODO
+    //   All fields are private and final. No mutator methods are provided.
     
-    // TODO constructor
-    
-    // TODO checkRep
-    
-    // TODO methods
-    
-    // TODO toString()
+    /**
+     * Constructor for Edge.
+     *
+     * @param source The source vertex.
+     * @param target The target vertex.
+     * @param weight The weight of the edge.
+     */
+    Edge(String source, String target, int weight) {
+        this.source = source;
+        this.target = target;
+        this.weight = weight;
+    }
+
+    /**
+     * Validates the representation invariant.
+     */
+    private void checkRep() {
+        assert source != null && !source.isEmpty() : "Invalid source vertex";
+        assert target != null && !target.isEmpty() : "Invalid target vertex";
+        assert weight > 0 : "Weight should be a positive integer";
+    }
+
+    /**
+     * Retrieves the source vertex of the edge.
+     *
+     * @return The source vertex.
+     */
+    public String getSource() {
+        return source;
+    }
+
+    /**
+     * Retrieves the target vertex of the edge.
+     *
+     * @return The target vertex.
+     */
+    public String getTarget() {
+        return target;
+    }
+
+    /**
+     * Retrieves the weight of the edge.
+     *
+     * @return The weight.
+     */
+    public int getWeight() {
+        return weight;
+    }
+      
+    /**
+     * Provides a string representation of the edge.
+     *
+     * @return String representation of the edge.
+     */
+    @Override
+    public String toString() {
+        return "(" + source + " -> " + target + ", weight: " + weight + ")";
+    }
     
 }
--- ps2/test/graph/GraphInstanceTest.java
+++ ps2/test/graph/GraphInstanceTest.java
@@ -20,7 +20,11 @@ import org.junit.Test;
 public abstract class GraphInstanceTest {
     
     // Testing strategy
-    //   TODO
+    //   For each method in the Graph interface, we'll test various aspects:
+    //   - Adding and removing vertices
+    //   - Adding and removing edges
+    //   - Setting and retrieving weights
+    //   - Modifying the graph (combining different operations)
     
     /**
      * Overridden by implementation-specific test classes.
@@ -36,11 +40,124 @@ public abstract class GraphInstanceTest {
     
     @Test
     public void testInitialVerticesEmpty() {
-        // TODO you may use, change, or remove this test
+        // Testing strategy: Empty graph should have no vertices initially
+        // Description: Asserts that an empty graph has no vertices using the vertices() method
         assertEquals("expected new graph to have no vertices",
                 Collections.emptySet(), emptyInstance().vertices());
     }
     
-    // TODO other tests for instance methods of Graph
+    // Test adding vertices to the graph
+    @Test
+    public void testAddVertices() {
+        // Testing strategy: Add vertices to an empty graph
+        // Description: Adds vertices to an empty graph and verifies their presence
+        Graph<String> graph = emptyInstance();
+        graph.add("A");
+        graph.add("B");
+        assertTrue(graph.vertices().contains("A"));
+        assertTrue(graph.vertices().contains("B"));
+        assertEquals(2, graph.vertices().size());
+    }
     
+    // Test removing vertices from the graph
+    @Test
+    public void testRemoveVertices() {
+        // Testing strategy: Remove vertices from a graph
+        // Description: Adds vertices, removes one, and checks if the removed vertex is absent
+        Graph<String> graph = emptyInstance();
+        graph.add("A");
+        graph.add("B");
+        graph.remove("A");
+        assertFalse(graph.vertices().contains("A"));
+        assertTrue(graph.vertices().contains("B"));
+        assertEquals(1, graph.vertices().size());
+    }
+    
+    // Test existence of vertices in the graph
+    @Test
+    public void testVerticesExistence() {
+        // Testing strategy: Verify if vertices exist in the graph
+        // Description: Adds vertices to a graph and checks for their existence
+        Graph<String> graph = emptyInstance();
+        graph.add("A");
+        graph.add("B");
+        assertTrue(graph.hasVertex("A"));
+        assertTrue(graph.hasVertex("B"));
+        assertFalse(graph.hasVertex("C"));
+    }
+    
+    // Test adding edges to the graph
+    @Test
+    public void testAddEdges() {
+        // Testing strategy: Add edges between vertices in the graph
+        // Description: Adds vertices and establishes an edge between them with a specific weight
+        Graph<String> graph = emptyInstance();
+        graph.add("A");
+        graph.add("B");
+        graph.set("A", "B", 5);
+        assertTrue(graph.vertices().contains("A"));
+        assertTrue(graph.vertices().contains("B"));
+        assertTrue(graph.hasEdge("A", "B"));
+        assertEquals(5, graph.getWeight("A", "B"));
+    }
+    
+    // Test removing edges from the graph
+    @Test
+    public void testRemoveEdges() {
+        // Testing strategy: Remove edges between vertices
+        // Description: Adds vertices, establishes an edge, removes it, and checks if the edge is absent
+        Graph<String> graph = emptyInstance();
+        graph.add("A");
+        graph.add("B");
+        graph.set("A", "B", 5);
+        graph.remove("A", "B");
+        assertFalse(graph.hasEdge("A", "B"));
+    }
+    
+    // Test existence of edges in the graph
+    @Test
+    public void testEdgesExistence() {
+        // Testing strategy: Check if edges exist between specific vertices
+        // Description: Adds vertices and checks for the existence of edges between them
+        Graph<String> graph = emptyInstance();
+        graph.add("A");
+        graph.add("B");
+        assertFalse(graph.hasEdge("A", "B"));
+    }
+    
+    // Test setting and getting weights for edges
+    @Test
+    public void testSetAndGetWeights() {
+        // Testing strategy: Set and retrieve weights for edges
+        // Description: Adds vertices, sets weights for edges, and retrieves those weights
+        Graph<String> graph = emptyInstance();
+        graph.add("A");
+        graph.add("B");
+        graph.set("A", "B", 5);
+        assertEquals(5, graph.getWeight("A", "B"));
+        
+        // Test updating weight
+        graph.set("A", "B", 10);
+        assertEquals(10, graph.getWeight("A", "B"));
+    }
+    
+    // Test various graph modifications
+    @Test
+    public void testGraphModification() {
+        // Testing strategy: Perform various modifications to the graph
+        // Description: Adds vertices, establishes edges, removes vertices and edges
+        // Verifies the graph's integrity after these operations
+        Graph<String> graph = emptyInstance();
+        graph.add("A");
+        graph.add("B");
+        graph.add("C");
+        graph.set("A", "B", 5);
+        graph.set("B", "C", 3);
+        graph.remove("A");
+        assertFalse(graph.hasVertex("A"));
+        assertTrue(graph.hasVertex("B"));
+        assertTrue(graph.hasVertex("C"));
+        assertFalse(graph.hasEdge("A", "B"));
+        assertTrue(graph.hasEdge("B", "C"));
+    }
 }
